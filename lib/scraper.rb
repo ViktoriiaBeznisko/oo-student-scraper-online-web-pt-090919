537484761da7009fd8c10ee9f28613699ada364b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-06 22:36:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,17 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    students = []
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/student-scraper-test-page/"))
-    doc.css("div.student-card")
+  # def self.scrape_index_page(index_url)
+  #   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/student-scraper-test-page/"))
+  #   students = []
+  #   #doc.css("div.student-card")
+  #   doc.search("div.student-card").each do |stud|
+  #     students << stud.text
+  # end
+
+
 
   def self.scrape_profile_page(profile_url)
 
   end
-
 end
